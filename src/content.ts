895ff57e1d76a7a5d0f0e1bf40a267971bab6a64
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-05-26 14:26:48 +0300

    changes

---
+++ src/content.ts
@@ -0,0 +1,86 @@
+import { after } from "spitroast";
+//import "./webpackGrabber.js";
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const { WEBPACK_GRABBER } = window; // you can cry typescript
+  const React = WEBPACK_GRABBER.findByProps("useState");
+  let module = WEBPACK_GRABBER.find(m=>m?.rs);
+  // ik this is terrible once I get it to work I will replace with saner search
+  let module3 = WEBPACK_GRABBER.find(m=>m?.resetIsModalScrollerRendered)
+  let tweetsModule = WEBPACK_GRABBER.find(m=>m?.Z?.WrappedComponent?.prototype?.render?.toString().includes("isRestrictedSession:e"))
+  let linkModule = WEBPACK_GRABBER.find(m=>m?.Z?.prototype?.render?.toString().includes("childrenStyle:w.flexGrow"))
+  let contentModuke = WEBPACK_GRABBER.find(m=>m?.nO)
+
+  /* this module is lazy loaded
+  before("render", linkModule.Z.prototype, (args) =>{
+    console.log("render2", linkModule.Z.prototype);
+  })
+  */
+
+  after("render", module.AW.prototype, (args, response) =>{
+    console.log("awk",response);
+    if (response?.children?.length == 11) {
+      console.log("found <Route>", response);
+    }
+  })
+
+  after("render", contentModuke.nO.prototype, (args, response) =>{
+    console.log("content", response);
+
+    if (response?._owner?.memoizedProps?.namespace && response._owner.memoizedProps.namespace.page == 'profile') {
+      console.log("found profile",response._owner.memoizedProps.namespace);
+    }
+
+    if (response?._owner?.memoizedProps?.namespace && response._owner.memoizedProps.namespace.page == 'profile' && response._owner.memoizedProps.section == 'gamers') {
+      console.log("found gamers");
+      response.children = React.createElement("div", {style: {display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%"}}, "Gamers");
+      return response;
+    }
+  })
+
+  after("render", tweetsModule.Z.WrappedComponent.prototype, (args, response) =>{
+    console.log("render", tweetsModule.Z.WrappedComponent.prototype);
+    console.log(response);
+    /*
+    if (document.URL.includes("gamers")) {
+      return React.createElement("div", {style: {display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%"}}, "Gamers");
+    }
+    */
+  })
+
+  // F0 is React Router
+  // EN is withRouter
+  // AW is possibly <Route>
+  after("render", module.F0.prototype, (args, response)  =>{
+
+    response.props.children.props.children.props.children.props.children[1].props.children.props.children.props.children.forEach(element => {
+      if (Array.isArray(element)) {
+        element.forEach(element2 => {
+
+          if (element2?.key?.includes("(likes|media")) {
+            element2.key = "(likes|media|gamers)";
+            element2.props.path = element2.props.path.replace("(likes|media)","(likes|media|gamers)");
+            //element2.props.component = React.createElement("div", {style: {display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%"}}, "Gamers");
+
+            // here I add my custom route to the array so whenever you call /reviews it will open twitter user's profile
+            console.log(element2)
+            console.log("added custom route");
+          }
+        });
+      } else {
+        //console.log("found route", element?.key);
+      }
+    });
+  })
+
+  /*
+  after("render", module2, (args, response)  =>{
+    console.log("render2", args);
+    console.log(response);
+  })
+  */
+
+  console.log("content loaded");
+
+});
+
---
+++ src/webpackGrabber.js
@@ -0,0 +1,180 @@
+;(function() {
+    function script() {
+        const values = o => (Array.isArray(o) ? o : Object.values(o));
+
+        Object.defineProperty(Function.prototype, "m", {
+            set(v) {
+                const source = this.toString();
+                if (
+                    source.includes("exports") &&
+                    (source.includes("false") || source.includes("!1")) &&
+                    !(Array.isArray(v) && v?.some(m => m.toString().includes("CHROME_WEBSTORE_EXTENSION_ID"))) // react devtools
+                ) {
+                    window.WEBPACK_GRABBER = {
+                        require: this,
+                        get cache() {
+                            if (!this.require.c) {
+                            const cache = this.require.c = {};
+                            for (const id in this.modules) {
+                                cache[id] = {
+                                id,
+                                loaded: true,
+                                exports: this.require(id)
+                                };
+                            }
+                            }
+                            return this.require.c;
+                        },
+                        get modules() {
+                            //return webpackChunk_twitter_responsive_web;
+                            return this.require.m;
+                        },
+                        get entryPoint() {
+                            return this.require.s;
+                        },
+                        get path() {
+                            return this.require.p;
+                        },
+                        get getDefaultExport() {
+                            return this.require.n;
+                        },
+                        cacheValues() {
+                            return values(this.cache);
+                        },
+                        moduleValues() {
+                            return values(this.modules);
+                        },
+
+                        find(filter) {
+                            const values = this.cacheValues();
+                            for (const { exports } of values) {
+                                if (exports && filter(exports)) return exports;
+                                if (exports?.default && filter(exports.default)) return exports.default;
+                                if (typeof exports === "object" && exports !== window) {
+                                    // Mangled exports
+                                    for (const key in exports) {
+                                        if (key.length > 3 || !exports[key]) continue;
+                                        if (filter(exports[key])) return exports[key];
+                                    }
+                                }
+                            }
+                            return null;
+                        },
+
+                        findAll(filter) {
+                            const results = [];
+                            const values = this.cacheValues();
+                            for (const { exports } of values) {
+                                if (exports && filter(exports)) results.push(exports);
+                                else if (exports?.default && filter(exports.default)) results.push(exports.default);
+                                if (typeof exports === "object" && exports !== window) {
+                                    // Mangled exports
+                                    for (const key in exports) {
+                                        if (key.length > 3 || !exports[key]) continue;
+                                        if (filter(exports[key])) {
+                                            results.push(exports[key]);
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            return results;
+                        },
+
+                        filters: {
+                            byProps:
+                                (...props) =>
+                                m =>
+                                    props.every(p => m[p] !== void 0),
+                            byDisplayName: displayName => m => m.displayName === displayName,
+                            byName: name => m => m.name === name,
+                            byCode:
+                                (...codes) =>
+                                m => {
+                                    if (typeof m !== "function") return false;
+                                    const code = Function.prototype.toString.call(m);
+                                    return codes.every(c => {
+                                        if (typeof c === "string") return code.includes(c);
+                                        if (c instanceof RegExp) {
+                                            const matches = c.test(code);
+                                            c.lastIndex = 0;
+                                            return matches;
+                                        }
+                                        throw new Error("findByCode: Expected one or more RegExp or string, got " + typeof c);
+                                    });
+                                },
+                        },
+
+                        findByProps(...props) {
+                            return this.find(this.filters.byProps(...props));
+                        },
+                        findAllByProps(...props) {
+                            return this.findAll(this.filters.byProps(...props));
+                        },
+                        findByDisplayName(displayName) {
+                            return this.find(this.filters.byDisplayName(displayName));
+                        },
+                        findAllByDisplayName(displayName) {
+                            return this.findAll(this.filters.byDisplayName(displayName));
+                        },
+                        findByName(name) {
+                            return this.find(this.filters.byName(name));
+                        },
+                        findAllByName(name) {
+                            return this.findAll(this.filters.byName(name));
+                        },
+                        findByCode(...codes) {
+                            return this.find(this.filters.byCode(...codes));
+                        },
+                        findAllByCode(...codes) {
+                            return this.findAll(this.filters.byCode(...codes));
+                        },
+                        findId(...codes) {
+                            const filter = this.filters.byCode(...codes);
+                            for (const id in this.modules) {
+                                if (filter(this.modules[id])) return id;
+                            }
+                            return null;
+                        },
+                        findModuleBySourceCode(...codes) {
+                            const id = this.findId(...codes);
+                            return id && this.require(id);
+                        },
+                    };
+
+                    delete Function.prototype.m;
+                    this.m = v;
+                    console.info(
+                        "%c%s%c %s %c%s",
+                        "background-color: #babbf1; color: black; border-radius: 4px; padding: 2px 4px; font-weight: bold;",
+                        "WebpackGrabber",
+                        "",
+                        "Found webpack_require! Check out",
+                        "font-weight: 600",
+                        "window.WEBPACK_GRABBER"
+                    );
+                } else {
+                    // huh not webpack_require
+                    Object.defineProperty(this, "m", {
+                    value: v,
+                    configurable: true,
+                    writable: true,
+                    enumerable: true
+                    });
+                }
+            },
+            configurable: true,
+        });
+        console.log("Injection successful");
+    }
+
+    function inject(fn) {
+      const script = document.createElement('script')
+      script.text = `(${fn.toString()})();`
+      document.documentElement.appendChild(script)
+    }
+
+    inject(script)
+  })()
+
+console.log("Webpack Grabber loaded");
