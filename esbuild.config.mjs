154455b0ad954ad3155dcbed1b3bfc1314c36483
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-05-27 12:39:59 +0300

    switch to esbuild

---
+++ esbuild.config.mjs
@@ -0,0 +1,18 @@
+import * as esbuild from 'esbuild'
+
+let ctx = await esbuild.context({
+  entryPoints: ['./src/content.ts'],
+  bundle: true,
+  outfile: './build/contentScript.js',
+})
+
+
+// this DOESNT EVEN PRINT OUTPUT
+await ctx.watch().then((p) => {
+  console.log('Build succeeded. Watching for changes...')
+}).catch((err) => {
+  console.error(err)
+})
+
+
+console.log('Watching for changes...')
