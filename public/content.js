fc4b0435606fa6674ae35f57a9aa69ba152dc011
V <vendicated@riseup.net>
2023-05-31 00:26:50 +0200

    content.js: Do not insert script twice

--- public/content.js
+++ public/content.js
@@ -2,7 +2,6 @@ const script = document.createElement("script");
 script.src = chrome.runtime.getURL("bundle.js");
 script.id = "review-db-script";
 script.dataset.extensionId = chrome.runtime.id;
-document.documentElement.append(script);
 
 const style = document.createElement("link");
 style.type = "text/css";
@@ -10,7 +9,6 @@ style.rel = "stylesheet";
 style.href = chrome.runtime.getURL("bundle.css");
 
 document.documentElement.append(script);
-
 document.addEventListener("DOMContentLoaded", () => document.documentElement.append(style), {
     once: true,
 });
