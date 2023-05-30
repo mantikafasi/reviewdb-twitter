7659473a293fc24c8ffba8045dbe62cc6a55b4ec
V <vendicated@riseup.net>
2023-05-30 02:16:38 +0200

    WHAT IS THIS FORMATTING MANTIKA

--- public/auth.js
+++ public/auth.js
@@ -2,6 +2,6 @@ response = document.body.innerText;
 
 const browser = chrome || browser;
 
-browser.runtime.sendMessage({ type: "setToken", token: "response.token" }, function(response) {
+browser.runtime.sendMessage({ type: "setToken", token: "response.token" }, function (response) {
     document.body.innerText = response.message;
 });
