e982fb045ccdaacfbea377a76875c541c67e9599
V <vendicated@riseup.net>
2023-05-30 15:10:47 +0200

    Fix freezing twitter on reload part 2

--- src/webpack/patchWebpack.ts
+++ src/webpack/patchWebpack.ts
@@ -29,6 +29,7 @@ if (window[WEBPACK_CHUNK]) {
     patchPush();
 } else {
     Object.defineProperty(window, WEBPACK_CHUNK, {
+        get: () => webpackChunk,
         set: v => {
             if (v?.push !== Array.prototype.push) {
                 console.info(`Patching ${WEBPACK_CHUNK}.push`);
